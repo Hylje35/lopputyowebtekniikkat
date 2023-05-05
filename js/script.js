6449bd392db34342b9847f68b040e4611e05e8b8
Hylje35 <lassitoriseva35@gmail.com>
2023-05-05 11:49:31 +0300

    js muokkaus

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 
 
-document.getElementById("kommentoi").addEventListener("click", addComment);
+document.getElementById("kommentoi").addEventListener("click", addComment); // lisätään kuuntelija kommentoi buttonille, klikkauksesta suoritetaan addComment funktio
 
-var elements = document.querySelector("#respond");
-let checkbox = document.getElementById("important");
-
+var elements = document.querySelector("#respond");  //haetaan elementit h5 input textarea yms//
+/* !!!!!!!!let checkbox = document.getElementById("#important");  
+!!!!!!!!!!!jos important checkbox olisi käytössä niin tällä haetaan sen tieto
+!!!!!!!!!!!sen jälkeen if lausekkeella määritellään lisätäänkö borderia tai punasta väriä tai mitä tahansa muuta mitä on css tiedostoon määritelty.*/
 function addComment(){
     let div = document.createElement("div");
     let nimi = document.createElement("h3");
     let kommentti = document.createElement("p");
     let br = document.createElement("br");
 
-    let date = new Date();
+    const date = new Date();
 
     // The toLocaleString() method returns a string with a language-sensitive representation of this date.//
     nimi.textContent = date.toLocaleString() + " " + user.value;
